969b94aaceaba37dc2c5f6b547bd8804d5f8559d
Gowri <prakashgowri72@gmail.com>
2023-08-15 13:40:25 -0400

    Date Table and Category Entity Completed

--- app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -3,20 +3,20 @@
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
 
-import { Heading } from "@/components/ui/heading"
+import { Heading }  from "@/components/ui/heading"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
-import { BillboardColumn, columns } from "./columns"
+import { CategoryColumn, columns } from "./columns"
 import { DataTable } from "@/components/ui/data-table"
 import { ApiList } from "@/components/ui/api-list"
 
 
-interface BillboardsClientProps {
-  data: BillboardColumn[]
+interface CategoryClientProps {
+  data: CategoryColumn[]
 }
 
 
-export const BillboardsClient: React.FC<BillboardsClientProps> = ({
+export const CategoryClient: React.FC<CategoryClientProps> = ({
   data
 }) => {
     const router = useRouter();
@@ -26,19 +26,19 @@ export const BillboardsClient: React.FC<BillboardsClientProps> = ({
     <>
     <div className="flex items-center justify-between">
         <Heading
-            title={`Billboards (${data.length})`}
-            description = "Manage billboards for your store"
+            title={`Categories (${data.length})`}
+            description = "Manage categories for your store"
         />
-        <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+        <Button onClick={() => router.push(`/${params.storeId}/categories/new`)}>
             <Plus className="mr-2 h-4 w-4"/>
             Add New
         </Button>
     </div>
     <Separator/>
-    <DataTable columns={columns} data={data} searchKey="label"/>
-    <Heading title="API" description="API calls for Billboards"/>
+    <DataTable columns={columns} data={data} searchKey="name"/>
+    <Heading title="API" description="API calls for Categories"/>
     <Separator/>
-    <ApiList entityName="billboards" entityIdName="billboardId"/>
+    <ApiList entityName="categories" entityIdName="categoryId"/>
     </>
   )
 }
